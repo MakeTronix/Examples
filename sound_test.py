8a74fb9dde2cbca121063c556ae362b2e401f898
ZacharyIgielman <zacharyi123@me.com>
2016-09-01 18:06:53 +0100

    correct rpilarm to alarm

--- sound_test.py
+++ sound_test.py
@@ -3,18 +3,18 @@
 # beeps five times
 # Author : Zachary Igielman
 
-#import neccesary libraries (that tell python how to interact with time and RPiLarm)
+#import neccesary libraries (that tell python how to interact with time and the board)
 import time, Alarm, sys
 
-#set up the connection to RPiLarm
-RPiLarm.init()
+#set up the connection to the board
+Alarm.init()
 
 #repeat 5 times
 for n in range(5):
     #turn the buzzer on for one second and off for one second
-    RPiLarm.sound(1)
+    Alarm.sound(1)
     time.sleep(1)
 
 #stop the script
-RPiLarm.cleanup()
+Alarm.cleanup()
 sys.exit(0)
