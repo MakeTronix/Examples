7558534333571a514ce891c9de150f8fcacfbcea
ZacharyIgielman <zacharyi123@me.com>
2016-09-01 18:04:49 +0100

    correct rpilarm to alarm

--- keypad_test.py
+++ keypad_test.py
@@ -3,17 +3,17 @@
 # waits for a key to be pressed and prints the numerical value
 # Author : Zachary Igielman
 
-#import neccesary libraries (that tell python how to interact with time and RPiLarm)
+#import neccesary libraries (that tell python how to interact with time and the board)
 import time, Alarm, sys
 
-#set up the connection to RPiLarm
-RPiLarm.init()
+#set up the connection to the board
+Alarm.init()
 
 #call the getCode() function from the library that waits for a key and returns its value
-k=RPiLarm.getCode()
+k=Alarm.getCode()
 #print the value
 print k
 
 #stop the script
-RPiLarm.cleanup()
+Alarm.cleanup()
 sys.exit(0)
