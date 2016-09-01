6c00d6bdc095387a4f2def3aa530b99437ad440a
ZacharyIgielman <zacharyi123@me.com>
2016-09-01 18:06:26 +0100

    correct rpilarm to alarm

--- motion_test.py
+++ motion_test.py
@@ -3,22 +3,22 @@
 # waits for 10 seconds and then waits until motion, when motion detected, prints 'alarm'
 # Author : Zachary Igielman
 
-#import neccesary libraries (that tell python how to interact with time and RPiLarm)
+#import neccesary libraries (that tell python how to interact with time and the board)
 import time, Alarm, sys
 
-#set up the connection to RPiLarm
-RPiLarm.init()
+#set up the connection to the board
+Alarm.init()
 
 #wait for 10 seconds
 time.sleep(10)
 
 #wait for motion
-while not RPiLarm.getMotion():
+while not Alarm.getMotion():
     #check for motion every second
     time.sleep(1)
 #inform the user that motion has been detected
 print 'alarm'
 
 #stop the script
-RPiLarm.cleanup()
+Alarm.cleanup()
 sys.exit(0)
