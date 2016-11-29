8a01fcf955100a896decf0a5aae84f291938847b
ZacharyIgielman <zacharyi123@me.com>
2016-11-29 19:31:01 +0000

    remove unwanted sys exit

--- motion_test.py
+++ motion_test.py
@@ -4,7 +4,7 @@
 # Author : Zachary Igielman
 
 #import neccesary libraries (that tell python how to interact with time and the board)
-import time, Alarm, sys
+import time, Alarm
 
 #set up the connection to the board
 Alarm.init()
@@ -21,4 +21,3 @@ print("alarm")
 
 #stop the script
 Alarm.cleanup()
-sys.exit(0)
